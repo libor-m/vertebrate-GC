6486e71ad6077d8a0930b9592260f9bacc386b10
Libor Morkovsky <morkovsk@natur.cuni.cz>
2015-08-21 15:21:09 +0200

    use reasonable version of base_counts

--- base_counts.py
+++ base_counts.py
@@ -1,70 +1,70 @@
 """input is 
-- gzipped fasta sequence
+- faidx indexed fasta sequence
 - window size
 
 output is
 - stats table: seq_name win_start win_end A C G T 
 
+Use pysam.Fastafile to access the indexed data.
+
 TODO: output in wig file, GC content
 """
 import sys
-import gzip
 import collections
 import itertools
+import pysam
 
+def fai(file):
+    """read and parse fai into a list of tuples
+    """
+    with open(file) as f:
+        lines = f.readlines()
+        return [tuple(l.strip().split('\t')) for l in lines]
+    
 def fasta(file):
-    """multifasta stream parser
-    for each sequence in file yields 
-    (name, details, seq_generator)
-    seq_generator returns the sequence base by base
+    """load index, 
+    use pysam to access all sequences mentioned in the index
     """
-    def catseq(file):
-        """return concatenated lines base by base
-        until empty line is found
-        """
-        for line in file:
-            line1 = line.strip()
-            if line1 == "":
-                return
-            for base in line1:
-                yield base
+    idx = fai(file + '.fai')
+    fa = pysam.Fastafile(file)
+    
+    for seq, _, _, _, _ in idx:
+        yield seq, seq, fa.fetch(seq)
 
-    for line in file:
-        if line[0] == '>':
-            seq_name = line.split()[0][1:]
-            yield (seq_name, line.strip()[1:], catseq(file))
-
-def windows(it, size):
-    """yield separate iterator for each size 
-    elements from it
-
-    TODO: use itertools tee to implemnt overlapping windows
-    combined with counter and subtractions
-    FIXME: this loses one base a time!
+def windows(data, size):
+    """walk a window along a sequecne
     """
-    for item in it:
-        yield itertools.islice(it, size)
+    for i in xrange(0, len(data) - size + 1, size):
+        yield i, data[i:(i + size)]
 
 def main():
     if len(sys.argv) < 2:
-        print >> sys.stderr, "use: %s fasta.gz [window_size=100K] [skip=window_size]" % sys.argv[0]
+        print >> sys.stderr, "use: %s faidxed_fasta [window_size=100K]" % sys.argv[0]
         sys.exit(1)
 
     if len(sys.argv) > 2:
         win_size = int(sys.argv[2])
     else:
         win_size = 100000
-    # TODO: overlapping windows
 
-    file = gzip.open(sys.argv[1])
-    for seq, _, data in fasta(file):
+    # TODO: overlapping windows
+    for seq, _, data in fasta(sys.argv[1]):
+        
+        # output current sequence name
+        sys.stderr.write(seq)
+        
         # walk by windows of given size
-        for i, win in enumerate(windows(data, win_size)):
+        for start, win in windows(data, win_size):
             # count different bases in window
             # TODO: add repeat masking obedient flag
-            c = collections.Counter(c.upper() for c in win)
+            try:
+            	c = collections.Counter(c.upper() for c in win)
+            except:
+                print >> sys.stderr, win
+            
             # print the results for each window
-            print "\t".join([seq, str(i * win_size)] + [str(c[base]) for base in ['A', 'C', 'G', 'T']])
+            print "\t".join([seq, str(start)] + [str(c[base]) for base in ['A', 'C', 'G', 'T']])
+            
             # progress indicator
             sys.stderr.write('.') 
 
